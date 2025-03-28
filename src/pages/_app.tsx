9dce94853e592bd5e4a687ac0677736f57ee15b7
yeeun <jovm268@naver.com>
2025-03-28 17:39:06 +0900

    cinema

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header>
+        <h1>ONEBITE CINEMA</h1>
+      </header>
+      <Component {...pageProps} />;
+    </>
+  );
 }
