4cf2d13199e40d0c588bfe6dea66ee4f936cee3b
yeeun-kor <jovm268@naver.com>
2025-03-30 16:00:07 +0900

    onecinema

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const onClickBtn = () => {
+    router.push("/test");
+  };
+  return (
+    <>
+      <Link href={"/movie"}>movie</Link>
+      <br />
+      <Link href={"/search"}>서칭페이지</Link>
+      <br />
+      <Link href={"/"}>홈페이지</Link>
+      <header>
+        <button onClick={onClickBtn}>/test페이지이동</button>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ src/test.tsx
@@ -0,0 +1,3 @@
+export default function test() {
+  return <h1>테스트페이지 입니다.</h1>;
+}
