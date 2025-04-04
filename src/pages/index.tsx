a6f75e69942e1953906b8a0d578f91a1e7e45298
yeeun <jovm268@naver.com>
2025-04-04 12:10:14 +0900

    ssr->ssg 변경 index.html

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,20 @@ import SearchBar from "./components/searchBar";
 import MovieItem from "./components/movie-item";
 import style from "./index.module.css";
 import fetchMovies from "@/lib/fetch-movies";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
 import fetchRandom from "@/lib/fetch-random";
 
 //ssr설정
-export const getServerSideProps = async () => {
+// export const getServerSideProps = async () => {
+//   const [allMovies, randomMovies] = await Promise.all([
+//     fetchMovies(),
+//     fetchRandom(),
+//   ]);
+//   return { props: { allMovies, randomMovies } };
+// };
+
+//SSG 설정
+export const getStaticProps = async () => {
   const [allMovies, randomMovies] = await Promise.all([
     fetchMovies(),
     fetchRandom(),
@@ -19,7 +28,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allMovies,
   randomMovies,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,28 +1,40 @@
 import GlobalSearchBar from "@/components/global-searchbar";
 import fetchMovies from "@/lib/fetch-movies";
 import MovieItem from "../components/movie-item";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
-import { ReactNode } from "react";
+import {
+  GetServerSidePropsContext,
+  GetStaticPropsContext,
+  InferGetServerSidePropsType,
+  InferGetStaticPropsType,
+} from "next";
+import { ReactNode, use, useEffect, useState } from "react";
+import { MovieData } from "@/type";
+import { useRouter } from "next/router";
+// SSR방식
+// export const getServerSideProps = async (
+//   context: GetServerSidePropsContext
+// ) => {
+//   const q = context.query.q;
+//   const movies = await fetchMovies(q as string);
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const q = context.query.q;
-  const movies = await fetchMovies(q as string);
+//   return { props: { movies } };
+// };
 
-  return { props: { movies } };
-};
+// SSG방식 -> 동적페이지
+export default function Search() {
+  const [movies, setMovies] = useState<MovieData[]>([]);
+  const router = useRouter();
+  const { q } = router.query;
 
-export default function Search({
-  movies,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return (
-    <div>
-      {movies.map((movie) => (
-        <MovieItem key={movie.id} {...movie}></MovieItem>
-      ))}
-    </div>
-  );
+  const fetchSearchResult = async () => {
+    const data = await fetchMovies(q as string);
+    setMovies(data);
+  };
+  useEffect(() => {
+    if (q) {
+      fetchMovies();
+    }
+  }, [q]);
 }
 
 Search.getLayout = (page: ReactNode) => {
