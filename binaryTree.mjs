d90b2ea8e8bd7ae50d6faa6ff98a1cb31cbd6dbe
yeeun-kor <jovm268@naver.com>
2025-06-02 13:14:19 +0900

    이진탐색트리(제거) 부가기능 설정 1. 왼쪽 자식 노드 제거 함수 생성 2. 오른쪽 자식 노드 제거 함수 생성

--- binaryTree.mjs
+++ binaryTree.mjs
@@ -61,6 +61,23 @@ class BinaryTree {
     //3. 루트노드방문
     console.log(tree.data);
   }
+
+  //- 왼쪽 자식 노드 제거하는 함수
+  removeLeftSubTree() {
+    //제거하는 노드는 변수로 반환 필요
+    let deletingNode = this.getLeftSubTree();
+    //그리고 왼쪽 자식노드를 null로 설정해서 끊어내기
+    this.getLeftSubTree(null);
+    //제거된 노드는 리턴하기
+    return deletingNode;
+  }
+  //- 오른쪽 자식 노드 제거하는 함수
+  removeRightSubTree() {
+    //제거하려는 노드는 변수로 반환 필요, 선택 해야하니깐
+    let deletingNode = this.getRightSubTree();
+    this.getRightSubTree(null);
+    return deletingNode;
+  }
 }
 
 export { BinaryTree };
