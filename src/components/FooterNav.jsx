ce1ffd9b03ca37e88a27f6b989c92cb3cddd07ae
yeeun <jovm268@naver.com>
2025-08-25 11:35:34 +0900

    chore: Footer 레이아웃 구조 설계

---
+++ src/components/FooterNav.jsx
@@ -0,0 +1,13 @@
+export default function FooterNav() {
+  return (
+    <div className="flex-col">
+      <h1 className="text-2xl">Service</h1>
+      <ul>
+        <li>cards</li>
+        <li>cards</li>
+        <li>cards</li>
+        <li>cards</li>
+      </ul>
+    </div>
+  );
+}
