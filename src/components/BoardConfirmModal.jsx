f8c3189bbbda296e206c08945382d5918b02f316
yeeun <jovm268@naver.com>
2025-08-27 01:17:19 +0900

    feat : useBoard 로 삭제 로직

--- src/components/BoardConfirmModal.jsx
+++ src/components/BoardConfirmModal.jsx
@@ -1,10 +1,12 @@
 //1. useBoardStore를 선언하여 zustand 스토어를 불러옵니다.
 //2. removeBoard 함수를 불러와 보드를 삭제 합니다.
 
-import React from 'react';
+import { useBoardStore } from '../store';
 
 const BoardConfirmModal = ({ onClose, id }) => {
+  const { removeBoard } = useBoardStore();
   const handleDelete = () => {
+    removeBoard(id);
     onClose();
   };
   return (
