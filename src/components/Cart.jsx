2233d759470a819c5d06618335b56e763dadcf47
yeeun <jovm268@naver.com>
2025-08-26 15:07:08 +0900

    refactor : Redux 라이브러리로 상태 관리

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,12 @@
+import { useDispatch, useSelector } from "react-redux";
 import data from "../assets/data";
+import { removeFromCart } from "../redux/redux";
 
-function Cart({ menu, cart, setCart }) {
+function Cart() {
+  //컴바인된 menu, cart 리덕스에서 가져오기
+  const menu = useSelector((state) => state.menuReducer);
+  const cart = useSelector((state) => state.cartReducer);
+  console.log(menu);
   if (!menu)
     return (
       <div style={{ textAlign: "center", margin: "80px" }}>
@@ -20,8 +26,6 @@ function Cart({ menu, cart, setCart }) {
               item={allMenus.find((menu) => menu.id === el.id)}
               options={el.options}
               quantity={el.quantity}
-              cart={cart}
-              setCart={setCart}
             />
           ))
         ) : (
@@ -32,7 +36,8 @@ function Cart({ menu, cart, setCart }) {
   );
 }
 
-function CartItem({ item, options, quantity, cart, setCart }) {
+function CartItem({ item, options, quantity }) {
+  const dispatch = useDispatch();
   return (
     <li className="cart-item">
       <div className="cart-item-info">
@@ -50,7 +55,7 @@ function CartItem({ item, options, quantity, cart, setCart }) {
       <button
         className="cart-item-delete"
         onClick={() => {
-          setCart(cart.filter((el) => item.id !== el.id));
+          dispatch(removeFromCart(item.id));
         }}
       >
         삭제
