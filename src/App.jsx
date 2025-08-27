da25ea3d829873cf8410b4eb4bdbdef4cc45942a
yeeun <jovm268@naver.com>
2025-08-27 16:01:30 +0900

    feat : 최적화 수정 (의존성배열 아직 헷갈림)

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
 import { memo, useCallback, useMemo, useState } from "react";
 import "./App.css";
 
-// 리스트 항목 컴포넌트
-const ListItem = memo(({ item, onClick }) => {
-  console.log(`Rendering ${item}`);
-  return <li onClick={() => onClick(item)}>{item}</li>;
-});
-
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedItem, setSelectedItem] = useState(null);
 
-  //items 배열을 useMemo를 사용하여 메모이제이션 (데이터 저장, 의존성배열 빈배열 , 초기 생성시만 생성)
+  //useMemo로 정적인 배열값 메모제이션 (저장) -> 의존성 빈배열
   const items = useMemo(() => {
     return ["Apple", "Banana", "Cherry", "Date", "Fig", "Grape"];
   }, []);
 
-  //3. `useMemo`를 사용하여 메모이제이션
+  //필터링된 값들 메모제이션 (저장) -> 아이템과 searchTerm 계속 바뀌니깐 의존성 배열에 넣어야함.
   const filteredItems = useMemo(() => {
     return items.filter((item) =>
       item.toLowerCase().includes(searchTerm.toLowerCase())
     );
   }, [items, searchTerm]);
 
-  //handleItemClick 함수를 useCallback으로 메모이제이션
+  //선택한 item이 변하지 않으면, 변하지 않겠다, 빈배열로 두기
   const handleItemClick = useCallback((item) => {
     setSelectedItem(item);
   }, []);
@@ -50,5 +44,11 @@ const App = () => {
     </div>
   );
 };
+// 리스트 항목 컴포넌트
+// eslint-disable-next-line react/display-name, react/prop-types
+const ListItem = memo(({ item, onClick }) => {
+  console.log(`Rendering ${item}`);
+  return <li onClick={() => onClick(item)}>{item}</li>;
+});
 
 export default App;
