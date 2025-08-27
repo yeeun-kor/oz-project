5ae53e8b417553302c0c8ef3cebe7be80c07ee4b
yeeun <jovm268@naver.com>
2025-08-27 13:19:22 +0900

    feat : useCallback, useMemo, React.memo 사용해보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import { memo, useCallback, useMemo, useState } from "react";
 import "./App.css";
 
 // 리스트 항목 컴포넌트
-const ListItem = ({ item, onClick }) => {
+const ListItem = memo(({ item, onClick }) => {
   console.log(`Rendering ${item}`);
   return <li onClick={() => onClick(item)}>{item}</li>;
-};
+});
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedItem, setSelectedItem] = useState(null);
 
-  const items = ["Apple", "Banana", "Cherry", "Date", "Fig", "Grape"];
+  //items 배열을 useMemo를 사용하여 메모이제이션 (데이터 저장, 의존성배열 빈배열 , 초기 생성시만 생성)
+  const items = useMemo(() => {
+    return ["Apple", "Banana", "Cherry", "Date", "Fig", "Grape"];
+  }, []);
 
-  const filteredItems = items.filter((item) =>
-    item.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  //3. `useMemo`를 사용하여 메모이제이션
+  const filteredItems = useMemo(() => {
+    return items.filter((item) =>
+      item.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }, [items, searchTerm]);
 
-  const handleItemClick = (item) => {
+  //handleItemClick 함수를 useCallback으로 메모이제이션
+  const handleItemClick = useCallback((item) => {
     setSelectedItem(item);
-  };
+  }, []);
 
   return (
     <div className="app-wrapper">
