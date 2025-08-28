1cc54c3e627199fc55300ca7f2816e053fc6f103
yeeun <jovm268@naver.com>
2025-08-28 12:06:37 +0900

    docs : rollup 번들러와 코드스플리팅 설정(viteConfig )

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Option 2: Function form for more dynamic control (e.g., all node_modules)
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Bundle all node_modules into a 'vendor' chunk
+          }
+        },
+      },
+    },
+  },
 });
