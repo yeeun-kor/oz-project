fcf98bb1b61b4a98bf07bf3421fb808efa69ea09
yeeun <jovm268@naver.com>
2025-08-29 12:08:46 +0900

    chore : prettier,eslint설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,25 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginReact.configs.flat.recommended,
+  {
+    extends: ["react-app", "eslint:recommended", "prettier"],
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      "no-var": "error", // var 금지
+      "no-multiple-empty-lines": "error", // 여러 줄 공백 금지
+      "no-console": ["error", { allow: ["warn", "error", "info"] }], // console.log() 금지
+      eqeqeq: "error", // 일치 연산자 사용 필수
+      "dot-notation": "error", // 가능하다면 dot notation 사용
+      "no-unused-vars": "error", // 사용하지 않는 변수 금지
     },
   },
-])
+]);
