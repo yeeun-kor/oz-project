b3c7da1ae94d11a9d2d4e8c7f8e4516e2fa7b01c
yeeun <jovm268@naver.com>
2025-09-01 14:18:40 +0900

    chore : vendoer파일 코드스플리팅

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id, { getModuleInfo, getModuleIds }) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
