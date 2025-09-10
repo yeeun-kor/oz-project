8aa06402dfd5ec90074034d3b5741c352da90cc0
L1m3Kun <sungku2757@gmail.com>
2025-09-10 13:30:41 +0900

    ♻️ Refactor: router & layout setting updated
    outlet added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import Layout from "./layouts/Layout";
 import useCart from "./hooks/useCart";
 import Home from "./pages/Home";
+import LoadingPage from "./pages/Loading";
 
 const Cart = lazy(() => import("./pages/Cart"));
-// 라우터를 이용하여 Home과 Cart를 연결해주세요
-// Cart는 처음에 로딩될 필요가 없기 떄문에 여러분들이 배운 lazy를 이용해서 최적화를 해 주세요.
-function App() {
-    // 장바구니 상태와 관련 함수들을 커스텀 훅에서 가져옵니다
-    // 가져온 장바구니 상태에서 어떤 부분을 넘겨줘야 하는지 생각하고 해당 컴포넌트에 넘겨주세요
-    const cartState = useCart();
 
-    return (
-        <Router>
-            <Layout cartState={cartState}>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Routes>
-                        <Route path="/" element={<Home addToCart={cartState.addToCart} />} />
-                        <Route path="/cart" element={<Cart cartState={cartState} />} />
-                    </Routes>
-                </Suspense>
-            </Layout>
-        </Router>
-    );
+function App() {
+  const cartState = useCart();
+
+  return (
+    <Router>
+      <Suspense fallback={<LoadingPage />}>
+        <Routes>
+          <Route
+            path="/"
+            element={<Layout itemCount={cartState.getTotalItems()} />}
+          >
+            <Route index element={<Home addToCart={cartState.addToCart} />} />
+            <Route path="/cart" element={<Cart cartState={cartState} />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
